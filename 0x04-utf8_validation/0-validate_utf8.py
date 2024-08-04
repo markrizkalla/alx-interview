bf8cd8199289c2e71dc3ace85db1df5bd4ca6a6f
markrizkalla <mark.rizkalla1@gmail.com>
2024-08-04 14:54:19 +0300

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ def validUTF8(data):
     """Validate data is UTF-8 encoding"""
 
     for j in range(len(data)):
-        byte = data[j] & 0xFF
+        byte = data[j]
         n = 0
         for i in range(7, 0, -1):
             if (byte >> i) == 1:
